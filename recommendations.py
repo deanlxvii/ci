688bcc3c5bc14953bb78d5bb7a71fa824b9488cc
Dean <Dean.LXVII@gmail.com>
2010-09-25 18:00:04 +0200

    refactored into class Recommendation

--- recommendations.py
+++ recommendations.py
@@ -3,59 +3,44 @@ from ci.calc.maths import *
 from ci.data.critics import critics
 
 class Recommendation(object):
-	def __init__(self):
-		pass
+	"""
+	Make recommendation based on een system with objects, items and weights.
+	Every object has some items and every item has a weight
+	"""
+	def __init__(self, data):
+		self.format(data)
 
-	def read(self, data):
+	def format(self, data):
+		"""
+		Format data
+		"""
 		if type(data) == type(dict()):
 				self.preferences = data
 
+	def intersection(self, collection1, collection2):
+		_intersection = {}
+		for item in self.preferences[collection1]:
+			if item in self.preferences[collection2]:
+				_intersection[item] = 1
+		return _intersection
+
+	def empty(self,intersection):
+		if len(intersection) == 0:
+			return True
+		return False
+
 	def similarity(self, function, object1, object2):
-		return function(self.preferences, object1, object2)
-
-def intersection(preferences, collection1, collection2):
-	intersect = {}
-	for item in preferences[collection1]:
-		if item in preferences[collection2]:
-			intersect[item] = 1
-	return intersect
-
-def empty(intersection):
-	if len(intersection) == 0:
-		return True
-	return False
-
-
-def sim_distance(preferences,object1, object2):
-	"""Returns a distance-based similarity score for object1 and object2"""
-	
-	shared_items = intersection(preferences, object1, object2)
-	if empty(shared_items):
-		return 0
-
-	x = [preferences[object1][item] for item in shared_items]
-	y = [preferences[object2][item] for item in shared_items]
-	return 1/(1+euclidean(x,y)**2)
-
-def sim_pearson(preferences, object1, object2):
-	"""Returns the Pearson correlation coefficient for object1 and object2"""
-
-	shared_items = intersection(preferences, object1, object2)
-	if empty(shared_items):
-		return 0
-
-	x = [preferences[object1][item] for item in shared_items]
-	y = [preferences[object2][item] for item in shared_items]
-	return pearson(x,y)
+		shared_items = self.intersection(object1, object2)
+		if self.empty(shared_items):
+			return 0
+		weights1 = [self.preferences[object1][item] for item in shared_items]
+		weights2 = [self.preferences[object2][item] for item in shared_items]
+		return function(weights1, weights2)
 
 
 if __name__ == "__main__":
-	print sim_distance(critics,'Lisa Rose','Gene Seymour')
-	print sim_distance(critics,'Lisa Rose','Lisa Rose')
-
-	print sim_pearson(critics, 'Lisa Rose', 'Gene Seymour')
-	print '------------------------------------------------'
-	recommend = Recommendation()
-	recommend.read(critics)
-	print recommend.similarity(sim_distance, 'Lisa Rose', 'Gene Seymour')
-	print recommend.similarity(sim_pearson, 'Lisa Rose', 'Gene Seymour')
+	recommend = Recommendation(critics)
+	print 1/(1+recommend.similarity(euclidean, 'Lisa Rose', 'Gene Seymour')**2)
+	print 1/(1+recommend.similarity(euclidean, 'Lisa Rose', 'Lisa Rose')**2)
+	print recommend.similarity(pearson, 'Lisa Rose', 'Gene Seymour')
+	print recommend.similarity(pearson, 'Lisa Rose', 'Lisa Rose')
