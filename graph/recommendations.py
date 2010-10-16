c2f4cb196b47c50ba9c941ef99828e224ca39c7b
Dean <Dean.LXVII@gmail.com>
2010-10-16 12:42:06 +0200

    plot text

--- graph/recommendations.py
+++ graph/recommendations.py
@@ -26,8 +26,28 @@ class Recommendation(RecommendationData):
 		for id in self.ratings:
 			for item in self.ratings[id]:
 				if self.ratings[id][item] > max:
-					min = self.ratings[id][item]
-		return min
+					max = self.ratings[id][item]
+		return max
+
+
+	def graph_config(self, item1, item2 , x, y, plottype):
+		axis([0 ,self.max_score + 1, 0, self.max_score + 1])
+		xlabel(item1)
+		ylabel(item2)
+		plot(x,y,plottype)
+
+	def plot_text(self, x, y, txt):
+		double = []
+		step = -0.2
+		for i in range(0, len(txt)):
+			if (x[i],y[i]) in double:
+				p = (x[i],y[i]+step)
+				double += [p]
+				text(p[0],p[1], txt[i])
+			else:
+				text(x[i],y[i], txt[i])
+				double += [(x[i],y[i])]
+
 
 	def draw(self, item1, item2):
 		p1 = []
@@ -43,15 +63,29 @@ class Recommendation(RecommendationData):
 				ob = ob + [object]
 			except:
 				pass
-		axis([0 ,self.max_score + 1, 0, self.max_score + 1])
-		xlabel(item1)
-		ylabel(item2)
-		plot(p1,p2,'ro')
+		self.graph_config(item1,item2, p1, p2, 'ro')
+		self.plot_text(p1,p2, ob)
 		show()
 
+	def draw_items(self, object1, object2):
+		p1 = []
+		p2 = []
+		k1 = set(self.ratings[object1].keys())
+		k2 = set(self.ratings[object2].keys())
+		u = k1.intersection(k2)
+		items = list(u)
+
+		p1 = [self.ratings[object1][item] for item  in items]
+		p2 = [self.ratings[object2][item] for item  in items]
+		
+		self.graph_config(object1, object2, p1, p2, 'ro')
+		self.plot_text(p1,p2,items)
+		show()
 
 if __name__ == '__main__':
 	g = Recommendation(critics)
 	print g.preferences
 	print g.min_score, g.max_score
-	g.draw('Dupree','Snakes')
+	#g.draw('Dupree','Snakes')
+	#g.draw_items('Mick LaSalle','Gene Seymour')
+	g.draw_items('Jack Matthews','Lisa Rose')
